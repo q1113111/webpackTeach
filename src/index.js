99bed239c14901f8f314375b120bc7aa6db13faa
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2021-09-02 13:07:56 +0800

    babel和產生相對檔案

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ btn.addEventListener("click", () => {
   const a = parseInt(box.textContent, 10);
   box.textContent = a + 1;
 });
+
+class Test {
+  #a = 0
+}
+const newTest = new Test()
+console.log('newTestaa',newTest.a)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
@@ -18,14 +19,32 @@ module.exports = {
     compress: true,
     port: 9000,
   },
-  plugins: [new HtmlWebpackPlugin()],
-  // 要使用的套件
+  devtool:'source-map',
+  // 使用的套件配置
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "main.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename:'main.[hash].css'
+    }),
+  ],
+  // 輸出的模組檔案
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        // use: ['style-loader', "css-loader"], 會放在<style>標籤裡 幾乎不會用這個
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
     ],
   },
 };
